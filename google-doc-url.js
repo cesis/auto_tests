31dd2fafc70d92e3f42080cee2638714eee3018a
Cēsu Kultūras centrs <80533818+cesis@users.noreply.github.com>
2021-03-15 22:38:38 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1l2Q6iwDjHW-F18q_zRDITXabHmsEFf-155AOmf-wNPk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Txm0OE_HOSMxTf78Js5ae6aqcvXN0A75tm7smE7wo-E/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAcuI3TWMNiN00NKYWAtBE5BRoS31bOZ98';
